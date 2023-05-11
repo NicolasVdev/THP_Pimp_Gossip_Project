a396013474632b4aa8c7a3df0d3d3f7e984ff306
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-05-11 22:00:12 +0200

    final push

--- app/controllers/gossip_likes_controller.rb
+++ app/controllers/gossip_likes_controller.rb
@@ -2,9 +2,15 @@ class GossipLikesController < ApplicationController
   before_action :authenticate_user, only: [:create, :destroy]
 
   def create
+    @like = GossipLike.new(user: current_user, gossip_id: params[:gossip_id])
+     @like.save
+     redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @like = GossipLike.find_by(user: current_user, gossip_id: params[:gossip_id])
+    @like.destroy
+    redirect_back(fallback_location: root_path)
   end
 
   private
